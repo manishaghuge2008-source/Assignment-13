33f2a304e0dc5669d3f9cad7d922acfc72931a63
Manisha <manishaghuge2008@gmail.com>
2026-07-04 13:58:10 +0530

    Updated Assignment 13

--- 03.py
+++ 03.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-# a) Numbers from 0 to 20
 a = np.arange(0, 21, 1)
 
-# b) Even numbers from 10 to 50
+
 b = np.arange(10, 51, 2)
 
-# c) Numbers from 5 to 100 with step of 5
-c = np.arange(5, 101, 5)
 
-# Print the arrays
+c = np.arange(5, 101, 5)
+ 
 print("a) Numbers from 0 to 20:")
 print(a)
 
-print("\nb) Even numbers from 10 to 50:")
+print("\n b) Even numbers from 10 to 50:")
 print(b)
 
-print("\nc) Numbers from 5 to 100 with step of 5:")
+print("\n c) Numbers from 5 to 100 with step of 5:")
 print(c)
